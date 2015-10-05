25b557cde6779b28d5f303952bdecdc402943a7e
Matthew Bradbury <mattbradburyuk@Matthews-iMac.local>
2015-10-05 21:35:30 +0100

    inc commit

--- Encoder_test_Run.py
+++ Encoder_test_Run.py
@@ -1,11 +1,15 @@
 
 from Encoder_test import *
+import time
 
 print "Running Encoder_test"
-
-
-
 
 ET = Encoder_test()
 
 ET.setPWM([100,100])
+
+t_end = time.time() + 60
+
+while time.time() < t_end
+
+ET.setPWM([0,0])
